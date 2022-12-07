51ee3f870c4fd7740898b376f7a12a34dec3329b
sungwooHa <ttlhi10@gmail.com>
2022-12-08 02:00:20 +0900

    fmt & clippy

--- advent_of_code_2023/src/problem/day3.rs
+++ advent_of_code_2023/src/problem/day3.rs
@@ -5,10 +5,10 @@ use crate::{solution::Solution, util};
 
 pub struct Day3 {}
 
-fn convert_priority(compartments : char) -> u32 {
+fn convert_priority(compartments: char) -> u32 {
     if compartments.is_uppercase() {
         compartments as u32 - 'A' as u32 + 27
-    }else {
+    } else {
         compartments as u32 - 'a' as u32 + 1
     }
 }
@@ -40,61 +40,41 @@ impl Solution for Day3 {
         let input = util::file_reader("day3.txt".to_string())?;
 
         let mut sum: u32 = 0;
-        let result = match IntoIterator::into_iter(input) {
+        let mut iter = IntoIterator::into_iter(input);
+        loop {
+            let mut next;
+            match iter.next() {
+                Some(val) => next = val,
+                None => break,
+            };
+            let res: HashMap<char, _> = next.chars().map(|char| (char, true)).collect();
 
+            match iter.next() {
+                Some(val) => next = val,
+                None => break,
+            };
 
-            mut iter => loop {
-                let mut next;
-                match iter.next() {
-                    Some(val) => next = val,
-                    None => break,
-                };
-                let res : HashMap<char, _> = next.chars().map(|char| (char, true)).collect();
-                
-                match iter.next() {
-                    Some(val) => next = val,
-                    None => break,
-                };
+            let res: HashMap<char, _> = next
+                .chars()
+                .map(|char| (char, true))
+                .filter(|(char, _)| res.get(char).is_some())
+                .collect();
 
-                let res: HashMap<char, _> = next
-                    .chars()
-                    .map(|char| (char, true))
-                    .filter(|(char, _)| res.get(char).is_some())
-                    .collect();
+            match iter.next() {
+                Some(val) => next = val,
+                None => break,
+            };
 
-                    match iter.next() {
-                        Some(val) => next = val,
-                        None => break,
-                    };
+            let res: Vec<_> = next
+                .chars()
+                .filter(|char| res.get(char).is_some())
+                //.map(|char| char as u32)
+                .collect();
 
-                    let res: Vec<_> = next
-                    .chars()
-                    .filter(|char| res.get(char).is_some())
-                    //.map(|char| char as u32)
-                    .collect();
-
-                    sum += convert_priority(res[0]);
-            },
-        };
+            sum += convert_priority(res[0]);
+        }
 
         println!("{:?}", sum);
-
-
-        // for line in input {
-            
-        //     let first = &line[0..line.len() >> 1];
-        //     let second = &line[line.len() >> 1..line.len()];
-
-        //     let map: HashMap<char, _> = first.chars().map(|char| (char, true)).collect();
-        //     let res: Vec<_> = second
-        //         .chars()
-        //         .filter(|char| map.get(char).is_some())
-        //         //.map(|char| char as u32)
-        //         .collect();
-
-        //     sum += convert_priority(res[0]);
-        // }
-
         Ok("ok".to_string())
     }
 }
